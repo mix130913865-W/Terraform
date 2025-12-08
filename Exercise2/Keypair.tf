571d5df627d2f83c7812707e83d8931021cd54bf
russell1 <wtfbebez@gmail.com>
2025-12-08 22:27:59 +0800

    initial commit of terraform exercise

--- Exercise2/Keypair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "dove-key" {
-  key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAty8RYmILbYuJ1pA3jdSbH/Nw5TlXrekPb8DgZijqFW Russell_Wang@Russell"
-}
